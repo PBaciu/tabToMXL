5d5aef6fc206f386f0e23e00530d08b5eaffe517
Patrick <baciupat@my.yorku.ca>
2021-01-14 22:04:23 -0500

    Creating conflict for real

--- TabToMXL/src/main/java/TabToMXL/Main.java
+++ TabToMXL/src/main/java/TabToMXL/Main.java
@@ -5,5 +5,8 @@ public class Main {
 		System.out.println("Testing");		
 		System.out.println("Change 2");
 		System.out.println("Testing my branch");
+		for (int j = 0; j < 10; j++) {
+			System.out.println("Creating a conflict");
+		}
 	}
 }
