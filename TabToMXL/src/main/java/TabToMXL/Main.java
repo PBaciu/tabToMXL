77eb3109883cd2bb89b314e073e7e27231050b4f
Patrick <baciupat@my.yorku.ca>
2021-01-14 21:32:17 -0500

    Testing my new branch

--- TabToMXL/src/main/java/TabToMXL/Main.java
+++ TabToMXL/src/main/java/TabToMXL/Main.java
@@ -4,7 +4,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Testing");
 		System.out.println("Change");
-		System.out.println("Change 2");
-		System.out.println("KevKev");
+		System.out.println("Change 2");		
 	}
 }
