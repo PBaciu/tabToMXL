2fd5e90ad8c6bea24d869f55784746017eaaaf69
Patrick <baciupat@my.yorku.ca>
2021-01-14 22:07:33 -0500

    Fixed formatting

--- TabToMXL/src/main/java/TabToMXL/Main.java
+++ TabToMXL/src/main/java/TabToMXL/Main.java
@@ -5,9 +5,11 @@ public class Main {
 		System.out.println("Testing");		
 		System.out.println("Change 2");
 		System.out.println("Testing my branch");
+		
 		for (int i = 0; i < 10; i++) {			
 			System.out.println("git is fun");
-    }
+		}
+		
 		for (int j = 0; j < 10; j++) {
 			System.out.println("Creating a conflict");
 		}
