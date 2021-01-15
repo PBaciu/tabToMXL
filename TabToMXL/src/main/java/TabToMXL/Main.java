eeb513b62ffbb16adb2d9c182920117f357ed631
Patrick <baciupat@my.yorku.ca>
2021-01-14 21:54:10 -0500

    Changed main, added println to test branch.

--- TabToMXL/src/main/java/TabToMXL/Main.java
+++ TabToMXL/src/main/java/TabToMXL/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Testing");
 		System.out.println("Change");
-		System.out.println("Change 2");		
+		System.out.println("Change 2");
+		System.out.println("Testing my branch");
 	}
 }
