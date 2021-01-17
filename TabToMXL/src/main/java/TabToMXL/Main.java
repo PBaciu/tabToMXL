282fac94d6af826b9895b69a411b22f85a7285a3
Patrick <baciupat@my.yorku.ca>
2021-01-16 19:16:20 -0500

    Cleaned up main method

--- TabToMXL/src/main/java/TabToMXL/Main.java
+++ TabToMXL/src/main/java/TabToMXL/Main.java
@@ -2,16 +2,6 @@ package TabToMXL;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Testing");		
-		System.out.println("Change 2");
-		System.out.println("Testing my branch");
-		
-		for (int i = 0; i < 10; i++) {			
-			System.out.println("git is fun");
-		}
-		
-		for (int j = 0; j < 10; j++) {
-			System.out.println("Creating a conflict");
-		}
+
 	}
 }
