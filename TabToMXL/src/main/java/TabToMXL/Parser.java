aef555aeb48257d44254b9f71a9bb544bbcc9ce1
Patrick <baciupat@my.yorku.ca>
2021-01-18 11:52:00 -0500

    Added resources from musicmxl github. Created Parser.java

---
+++ TabToMXL/src/main/java/TabToMXL/Parser.java
@@ -0,0 +1,16 @@
+package TabToMXL;
+
+import java.nio.file.Path;
+
+/**
+ * The purpose of this class is to provide utility for parsing .txt files
+ */
+public class Parser {
+
+    private final Path path;
+
+    public Parser(Path path) {
+        this.path = path;
+    }
+
+}
