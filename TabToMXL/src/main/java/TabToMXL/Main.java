d909e84020e473819828988efceb135aa1720f50
Patrick <baciupat@my.yorku.ca>
2021-01-20 12:34:48 -0500

    Change for lab demo

--- TabToMXL/src/main/java/TabToMXL/Main.java
+++ TabToMXL/src/main/java/TabToMXL/Main.java
@@ -4,5 +4,7 @@ public class Main {
 	public static void main(String[] args) {
  		System.out.println("HELOOOOOO!!!");
  		System.out.println("new change");
+
+		System.out.println("This is the change that was made for our lab session on Jan 20, 2021");
 	}
 }
