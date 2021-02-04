89bc7c55b0fcf212ae66117ca6b8144398b1b596
Kevin Hua <kevhua36@my.yorku.ca>
2021-02-04 18:05:02 -0500

    Wondering how many classes do I need and if some classes are needed like the page layout in defaults.

--- TabToMXL/src/main/java/objects/Defaults.java
+++ TabToMXL/src/main/java/objects/Defaults.java
@@ -1,5 +1,15 @@
 package objects;
 
 public class Defaults {
-
+	private double millimeters = 6.99911;	//maybe put this in an object called scaling?
+	private int tenths = 40;
+	
+	private double pageHeight = 1596.77;
+	private double pageWidth = 1233.87;
+	
+	private String pageMargin[] = {"even", "odd"};
+	private double leftMargin = 85.7252;
+	private double rightMargin = 85.7252;
+	private double topMargin = 85.7252;
+	private double bottomMargin = 85.7252;
 }
