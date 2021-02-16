186e48a2a65bdbf91b032a971f24b8cf0322fd06
Ahmed Ali <ahmedomaraali@gmail.com>
2021-02-16 13:11:28 -0500

    Added Print statement to print "res"

--- TabToMXL/src/main/java/TabToMXL/Main.java
+++ TabToMXL/src/main/java/TabToMXL/Main.java
@@ -4,5 +4,6 @@ public class Main {
 	public static void main(String[] args) {
 		Parser p = new Parser("src/main/java/TabToMXL/Capricho_Arabe_Tab.txt");
 		var res = p.readTab();
+		System.out.println(res);
 	}
 }
