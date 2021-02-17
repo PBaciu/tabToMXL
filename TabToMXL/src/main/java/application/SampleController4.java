da731944c91ca5c562bf6372b564bebb25784961
Venkatessh Kumar <venki365@my.yorku.ca>
2021-02-17 15:47:53 +0400

    Sample4

---
+++ TabToMXL/src/main/java/application/SampleController4.java
@@ -0,0 +1,25 @@
+package application;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+import javafx.animation.FadeTransition;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
+
+public class SampleController4 implements Initializable {
+	
+	@FXML
+	private AnchorPane rootPane;
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+	}
+}
