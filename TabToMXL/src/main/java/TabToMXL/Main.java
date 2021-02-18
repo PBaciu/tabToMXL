695e09d8e35ea05f093b0e412e6a8c40e208481a
Patrick <baciupat@my.yorku.ca>
2021-02-17 20:56:24 -0500

    Small changes to main and build

--- TabToMXL/src/main/java/TabToMXL/Main.java
+++ TabToMXL/src/main/java/TabToMXL/Main.java
@@ -2,7 +2,8 @@ package TabToMXL;
 
 public class Main {
 	public static void main(String[] args) {
-		Parser p = new Parser("src/main/java/TabToMXL/Capricho_Arabe_Tab.txt");
-		var res = p.readTab();
+//		Parser p = new Parser("src/main/java/TabToMXL/Capricho_Arabe_Tab.txt");
+//		var res = p.readTab();
+		org.jfx.Main.main(args);
 	}
 }
